dad96f7d0b4d1042222f5d82fccd0734fb25e8ab
Owen Horn <owenhorn44@gmail.com>
2019-06-20 00:15:49 +0000

    Changed route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Hello, world!"
+    render html: "Hola, mundo!"
+  end
+  
+  def goodbye
+    render html: "Goodbye!"
   end
 end
